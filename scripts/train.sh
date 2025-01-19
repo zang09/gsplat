b616b4da9c3be4c6bcd65983970fbaeed4b83910
haebeom <zang09@naver.com>
2025-01-20 00:50:18 +0900

    update scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,9 +2,11 @@
 input_dir=$1
 output_dir=$2
 
+# --fix_lidar
 python ../examples/simple_trainer.py default \
     --data_dir ${input_dir} --data_factor 1 \
     --result_dir ${output_dir} \
     --init_type sfm+lidar \
     --masking \
-    --fix_lidar
+    --chunk \
+    --test_every 10
